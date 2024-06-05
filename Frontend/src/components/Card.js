2385da9f73619f6d46a5e92a97df7e292b0d8fc9
Samu <samupie@metropolia.fi>
2024-06-05 14:01:56 +0300

    Made the cards font responsive to screen size with monster useEffect function.

--- Frontend/src/components/Card.js
+++ Frontend/src/components/Card.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import CardFaceDown from "./CardFaceDown";
 
 const Card = (props) => {
   const { suit, value, isFaceUp } = props;
   const isRed = suit === "♥" || suit === "♦";
+  const cardValueRef = useRef(null);
+  const cardSuitRef = useRef(null);
+
+  useEffect(() => {
+    const resizeFont = () => {
+      [cardValueRef.current, cardSuitRef.current].forEach((element) => {
+        if (element) {
+          let fontSize = parseFloat(window.getComputedStyle(element).fontSize);
+          while (
+            element.scrollHeight > element.offsetHeight ||
+            element.scrollWidth > element.offsetWidth
+          ) {
+            fontSize--;
+            element.style.fontSize = fontSize + "px";
+          }
+        }
+      });
+    };
+
+    window.addEventListener("resize", resizeFont);
+    resizeFont();
+
+    return () => {
+      window.removeEventListener("resize", resizeFont);
+    };
+  }, [isFaceUp]);
+
   return (
     <>
       {isFaceUp ? (
         <>
           <div className="card-container">
-            <div className={`card-value ${isRed ? "red-text" : ""}`}>
+            <div
+              ref={cardValueRef}
+              className={`card-value ${isRed ? "red-text" : ""}`}
+            >
               {value}
             </div>
-            <div className={`card-suit ${isRed ? "red-text" : ""}`}>{suit}</div>
+            <div
+              ref={cardSuitRef}
+              className={`card-suit ${isRed ? "red-text" : ""}`}
+            >
+              {suit}
+            </div>
           </div>
         </>
       ) : (
@@ -21,4 +56,5 @@ const Card = (props) => {
     </>
   );
 };
+
 export default Card;
