4f5a6287aefeb913602b623244acbe0077c18ec0
Samu <samupie@metropolia.fi>
2024-06-06 18:55:36 +0300

    deleted Backend, its not yet ready for use.

--- Backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGO_URI;
-
-const connectDB = async () => {
-  const conn = await mongoose.connect(MONGO_URI);
-  console.log(`Connected to database`);
-};
-
-module.exports = connectDB;
